91d7da44268a0481dbf5f36f9f9ca560213edaf6
imaonirvana <dfun801@gmail.com>
2022-09-19 14:46:21 +0300

    User task

---
+++ User/User/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class User
+{
+    string log; 
+    string firstname;
+    string secondname;
+    int age;
+
+    readonly DateTime dateOfCreation;
+
+    public User(string log, string firstname, string secondname, int age)
+    {
+        this.log = log;
+        this.firstname = firstname;
+        this.secondname = secondname;
+        this.age = age;
+
+        this.dateOfCreation = DateTime.Now;
+    }
+
+    public string getInfo() {
+        return $"{log} {firstname} {secondname} {age}. Registered: {this.dateOfCreation.ToString()}";
+    }
+}
